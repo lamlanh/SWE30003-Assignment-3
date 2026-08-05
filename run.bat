4a81abe670639d8f6e07a440a8f637bb73515e7a
lucianle1024 <letuanhuy2410@gmail.com>
2026-08-05 22:06:41 +0700

    update script

--- run.bat
+++ run.bat
@@ -2,6 +2,11 @@
 echo  SmartFM - Setup and Run Script (Windows)
 echo.
 
+IF NOT EXIST ".venv\" (
+    echo Creating virtual environment...
+    python -m venv .venv
+)
+
 call .venv\Scripts\activate.bat
 
 echo [1/2] Installing required libraries from requirements.txt...
--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 echo "  SmartFM - Setup and Run Script (Linux/Mac)"
 echo ""
 
+if [ ! -d ".venv" ]; then                                                                             
+    echo "Creating virtual environment..."                                                            
+    python3 -m venv .venv                                                                             
+fi  
+
 source .venv/bin/activate
 
 echo "[1/2] Installing required libraries from requirements.txt..."
